54d92dde54ccf1e644fe135fa1b7606380bbda39
Manvi Tripathi <manvit542@gmail.com>
2026-08-26 21:15:14 +0530

    Valid anagram soln

---
+++ Arrays/Valid_anagram.cpp
@@ -0,0 +1,29 @@
+//Leetcode 242 (Valid anagram)
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() !=t.length())
+        {
+            return false;
+        }
+        vector<int> freq(26, 0);
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        
+        for (int i = 0; i < freq.size(); i++) {
+            if (freq[i] != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}; 
+
+//Time - O(n)
+// space - O(1)
