c1b75253ba413e5e8fb7c51ecb08007855969682
Manvi Tripathi <manvit542@gmail.com>
2026-09-01 20:17:14 +0530

    Delete middle node soln

---
+++ Linked_List/Delete_middle_node.cpp
@@ -0,0 +1,38 @@
+//Leetcode 2095 (Delete middle node of a LL)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head->next == nullptr)
+            return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* prev = nullptr;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        prev->next = slow->next;
+
+        return head;
+    }
+};
+
+/* Two pointer approach 
+  time = (n) one traversal
+  space = O(1) */
